34cfea16b7ddd16ad327b6f799f8b94a7af90f8b
Tim Jonas Meinerzhagen <34220101+timmeinerzhagen@users.noreply.github.com>
2022-12-05 15:37:09 +0000

    rm: print

--- mkdocs_issue_showcaser/plugin.py
+++ mkdocs_issue_showcaser/plugin.py
@@ -19,30 +19,23 @@ class IssueShowcaserPlugin(BasePlugin):
         self.enabled = True
 
     def on_startup(self, command, dirty):
-        print("START")
         self.github_token = environ.get(self.config['env_github_token'])
         self.repo = self.config['repo']
-        print("EXCEPTION")
             
         if not self.github_token:
             raise Exception("No GitHub token found in environment variable '{}'".format(self.config['env_github_token']))
         
-        print("REQ")
         g = Github(self.github_token)
-        print("PROC")
         for issue in g.get_repo(self.repo).get_issues():
-            print(issue)
             title = re.findall('`([^`]*)`', issue.title)
             body = []
             if issue.body:
                 body = re.findall('`([^`]*)`', issue.body)
 
-            print(title + body)
             for file in title + body:
                 if file not in self.issues:
                     self.issues[file] = []
                 self.issues[file].append(issue)
-        print("DONE")
 
     def on_page_markdown(self, markdown, page, config, files):
         if page.file.src_uri in self.issues:
