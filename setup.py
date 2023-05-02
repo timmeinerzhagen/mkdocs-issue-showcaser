31cb9f768b4563388aea0105c4fbce449c9b30e0
Tim Jonas Meinerzhagen <34220101+timmeinerzhagen@users.noreply.github.com>
2023-05-02 13:55:50 +0200

    chore: setup project

--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='devops-mkdocs-issue-showcaser',
-    version='0.0.1',
+    version='0.1.0',
     description='MkDocs plugin for setting showcasing GitHub Issues on the corresponding pages.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     keywords='mkdocs github issues',
-    url='https://github.com/bayer-int/devops-mkdocs-issue-showcaser',
+    url='https://github.com/timmeinerzhagen/mkdocs-issue-showcaser',
     author='Tim Jonas Meinerzhagen',
-    author_email='tim.meinerzhagen@bayer.com',
+    author_email='tim@meinerzhagen.me',
+    license='MIT',
+    license_files = ('LICENSE'),
+    classifiers=[
+        "Operating System :: OS Independent",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        "License :: OSI Approved :: MIT License",
+        'Topic :: Documentation',
+        'Topic :: Text Processing',
+    ],
     python_requires='>=3.4',
     install_requires=[
         'mkdocs>=0.17',
